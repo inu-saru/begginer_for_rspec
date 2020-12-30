af3318a88596b5272a648e79ea38c122731dda3a
inu_saru <inu_saru_eagle@yahoo.co.jp>
2020-12-30 23:43:27 +0900

    Add method to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,12 @@ class User < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, uniqueness: true
+
+  def name
+    "#{self.first_name} #{self.last_name}"
+  end
+
+  def self.all_names
+    User.all.map(&:name)
+  end
 end
