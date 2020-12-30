5f63f2ff563155cc8d2c56af9131b414e72cae68
inu_saru <inu_saru_eagle@yahoo.co.jp>
2020-12-31 02:10:54 +0900

    Create user factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :user do
-
+    first_name { FFaker::NameJA.first_name }
+    last_name { FFaker::NameJA.last_name }
+    email { FFaker::Internet.email }
   end
 end
