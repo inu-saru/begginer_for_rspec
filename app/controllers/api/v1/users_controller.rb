2d9542a08285a68e0ed47d68e803b857026777df
inu_saru <inu_saru_eagle@yahoo.co.jp>
2020-12-31 19:13:20 +0900

    Create users_controller#create

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -6,4 +6,18 @@ class Api::V1::UsersController < ApplicationController
       email: user.email
     }
   end
+
+  def create
+    user = User.create!(user_params)
+    render json: {
+      name: user.name,
+      email: user.email
+    }
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email)
+  end
 end
